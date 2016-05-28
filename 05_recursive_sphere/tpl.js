19941c26f27aeb47a338b537870f4ab10a7e1c0b
Dennis Sabelleck <dennis.sabelleck@gmx.net>
2016-05-28 19:42:25 +0200

    - add recursive sphere

---
+++ 05_recursive_sphere/tpl.js
@@ -0,0 +1,325 @@
+var app = ( function() {
+
+	var gl;
+
+	// The shader program object is also used to
+	// store attribute and uniform locations.
+	var prog;
+
+	// Array of model objects.
+	var models = [];
+	var iterations;
+	
+	var camera = {
+		// Initial position of the camera.
+		eye : [0, 1, 4],
+		// Point to look at.
+		center : [0, 0, 0],
+		// Roll and pitch of the camera.
+		up : [0, 1, 0],
+		// Opening angle given in radian.
+		// radian = degree*2*PI/360.
+		fovy : 60.0 * Math.PI / 180,
+		// Camera near plane dimensions:
+		// value for left right top bottom in projection.
+		lrtb : 2.0,
+		// View matrix.
+		vMatrix : mat4.create(),
+		// Projection matrix.
+		pMatrix : mat4.create(),
+		// Projection types: ortho, perspective, frustum.
+		projectionType : "ortho",
+		// Angle to Z-Axis for camera when orbiting the center
+		// given in radian.
+		zAngle : 0,
+		// Angle to X-Axis for camera when orbiting the center
+		// given in radian.
+		xAngle : 0,
+		// Distance in XZ-Plane from center when orbiting.
+		distance : 4,
+	};
+
+	function start(iterations) {
+		init(iterations);
+		render();
+	}
+
+	function init(iterations) {
+		initWebGL();
+		initShaderProgram();
+		initUniforms()
+		initModels(iterations);
+		initEventHandler();
+		initPipline();
+	}
+
+	function initWebGL() {
+		// Get canvas and WebGL context.
+		canvas = document.getElementById('canvas');
+		gl = canvas.getContext('experimental-webgl');
+
+		gl.viewportWidth = canvas.width;
+		gl.viewportHeight = canvas.height;
+	}
+
+	/**
+	 * Init pipeline parameters that will not change again.
+	 * If projection or viewport change, their setup must
+	 * be in render function.
+	 */
+	function initPipline() {
+		gl.clearColor(.95, .95, .95, 1);
+
+		// Backface culling.
+		// gl.frontFace(gl.CCW);
+		// gl.enable(gl.CULL_FACE);
+		// gl.cullFace(gl.BACK);
+
+		// Depth(Z)-Buffer.
+		gl.enable(gl.DEPTH_TEST);
+
+		// Polygon offset of rastered Fragments.
+		gl.enable(gl.POLYGON_OFFSET_FILL);
+		gl.polygonOffset(0.5, 0);
+
+		// Set viewport.
+		gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+
+		// Init camera.
+		// Set projection aspect ratio.
+		camera.aspect = gl.viewportWidth / gl.viewportHeight;
+	}
+
+	function initShaderProgram() {
+		// Init vertex shader.
+		var vs = initShader(gl.VERTEX_SHADER, "vertexshader");
+		// Init fragment shader.
+		var fs = initShader(gl.FRAGMENT_SHADER, "fragmentshader");
+		// Link shader into a shader program.
+		prog = gl.createProgram();
+		gl.attachShader(prog, vs);
+		gl.attachShader(prog, fs);
+		gl.bindAttribLocation(prog, 0, "aPosition");
+		gl.linkProgram(prog);
+		gl.useProgram(prog);
+	}
+
+	/**
+	 * Create and init shader from source.
+	 * 
+	 * @parameter shaderType: openGL shader type.
+	 * @parameter SourceTagId: Id of HTML Tag with shader source.
+	 * @returns shader object.
+	 */
+	function initShader(shaderType, SourceTagId) {
+		var shader = gl.createShader(shaderType);
+		var shaderSource = document.getElementById(SourceTagId).text;
+		gl.shaderSource(shader, shaderSource);
+		gl.compileShader(shader);
+		if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+			console.log(SourceTagId+": "+gl.getShaderInfoLog(shader));
+			return null;
+		}
+		return shader;
+	}
+
+	function initUniforms() {
+		// Projection Matrix.
+		prog.pMatrixUniform = gl.getUniformLocation(prog, "uPMatrix");
+
+		// Model-View-Matrix.
+		prog.mvMatrixUniform = gl.getUniformLocation(prog, "uMVMatrix");
+	}
+
+	function initModels(iterations) {
+		// fill-style
+		var fs = "fillwireframe";
+		createModel("sphere", fs, iterations);
+	}
+
+	/**
+	 * Create model object, fill it and push it in models array.
+	 * 
+	 * @parameter geometryname: string with name of geometry.
+	 * @parameter fillstyle: wireframe, fill, fillwireframe.
+	 */
+ function createModel(geometryname, fillstyle, iterations) {
+		var model = {};
+		model.fillstyle = fillstyle;
+		initDataAndBuffers(model, geometryname, iterations);
+		// Create and initialize Model-View-Matrix.
+		model.mvMatrix = mat4.create();
+
+		models.push(model);
+	}
+
+	/**
+	 * Init data and buffers for model object.
+	 * 
+	 * @parameter model: a model object to augment with data.
+	 * @parameter geometryname: string with name of geometry.
+	 */
+	function initDataAndBuffers(model, geometryname, iterations) {
+		// Provide model object with vertex data arrays.
+		// Fill data arrays for Vertex-Positions, Normals, Index data:
+		// vertices, normals, indicesLines, indicesTris;
+		// Pointer this refers to the window.
+		this[geometryname]['createVertexData'].apply(model, [iterations]);
+
+		// Setup position vertex buffer object.
+		model.vboPos = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, model.vboPos);
+		gl.bufferData(gl.ARRAY_BUFFER, model.vertices, gl.STATIC_DRAW);
+		// Bind vertex buffer to attribute variable.
+		prog.positionAttrib = gl.getAttribLocation(prog, 'aPosition');
+		gl.enableVertexAttribArray(prog.positionAttrib);
+
+		// Setup normal vertex buffer object.
+		model.vboNormal = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, model.vboNormal);
+		gl.bufferData(gl.ARRAY_BUFFER, model.normals, gl.STATIC_DRAW);
+		// Bind buffer to attribute variable.
+		prog.normalAttrib = gl.getAttribLocation(prog, 'aNormal');
+		gl.enableVertexAttribArray(prog.normalAttrib);
+
+		// Setup lines index buffer object.
+		model.iboLines = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.iboLines);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, model.indicesLines,
+			gl.STATIC_DRAW);
+		model.iboLines.numberOfElements = model.indicesLines.length;
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+		// Setup triangle index buffer object.
+		model.iboTris = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.iboTris);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, model.indicesTris,
+			gl.STATIC_DRAW);
+		model.iboTris.numberOfElements = model.indicesTris.length;
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+	}
+
+	function initEventHandler() {
+
+		window.onkeydown = function(evt) {
+			var key = evt.which ? evt.which : evt.keyCode;
+            var c = String.fromCharCode(key);
+			var delta = Math.PI / 36;
+            var keyLeft = 37;
+            var keyRight = 39;
+            var keyUp = 38
+            var keyDown = 40;
+			switch(key) {
+                case keyLeft:
+                    camera.zAngle += delta;
+                    break;
+                case keyRight:
+                    camera.zAngle -= delta;
+                    break;
+                case keyUp:
+                    camera.xAngle -= delta;
+                    break;
+                case keyDown:
+                    camera.xAngle += delta;
+                    break;
+            }
+
+			if(c == 'S')
+				camera.up[1] *= -1;
+
+			// Render the scene again on any key pressed.
+			render();
+		};
+	}
+
+	/**
+	 * Run the rendering pipeline.
+	 */
+	function render() {
+		// Clear framebuffer and depth-/z-buffer.
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+		setProjection();
+
+		// mat4.identity(camera.vMatrix);
+		calculateCameraOrbit();
+        mat4.lookAt(camera.vMatrix, camera.eye, camera.center, camera.up);
+
+		// Loop over models.
+		for(var i = 0; i < models.length; i++) {
+			// Update modelview for model.
+			mat4.copy(models[i].mvMatrix, camera.vMatrix);
+
+			// Set uniforms for model.
+			gl.uniformMatrix4fv(prog.mvMatrixUniform, false,
+				models[i].mvMatrix);
+			
+			draw(models[i]);
+		}
+	}
+
+    function calculateCameraOrbit() {
+        // Calculate x,z position/eye of camera orbiting the center.
+        var x = 0, y = 1, z = 2;
+        camera.eye[x] = camera.center[x];
+        camera.eye[y] = camera.center[y];
+        camera.eye[z] = camera.center[z];
+
+        camera.eye[x] += camera.distance * Math.sin(camera.zAngle);
+        camera.eye[z] += camera.distance * Math.cos(camera.zAngle);
+
+		camera.eye[y] += camera.distance * Math.sin(camera.xAngle);
+		camera.eye[x] = camera.eye[x] * Math.cos(camera.xAngle);
+		camera.eye[z] = camera.eye[z] * Math.cos(camera.xAngle);
+
+		var up = Math.cos(camera.xAngle) > 0 ? 1 : -1;
+		camera.up[y] = up;
+    }
+
+	function setProjection() {
+		// Set projection Matrix.
+		switch(camera.projectionType) {
+			case("ortho"):
+				var v = camera.lrtb;
+				mat4.ortho(camera.pMatrix, -v, v, -v, v, -10, 10);
+				break;
+		}
+		// Set projection uniform.
+		gl.uniformMatrix4fv(prog.pMatrixUniform, false, camera.pMatrix);
+	}
+
+	function draw(model) {
+		// Setup position VBO.
+		gl.bindBuffer(gl.ARRAY_BUFFER, model.vboPos);
+		gl.vertexAttribPointer(prog.positionAttrib,3,gl.FLOAT,false,0,0);
+
+		// Setup normal VBO.
+		gl.bindBuffer(gl.ARRAY_BUFFER, model.vboNormal);
+		gl.vertexAttribPointer(prog.normalAttrib,3,gl.FLOAT,false,0,0);
+
+		// Setup rendering tris.
+		var fill = (model.fillstyle.search(/fill/) != -1);
+		if(fill) {
+			gl.enableVertexAttribArray(prog.normalAttrib);
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.iboTris);
+			gl.drawElements(gl.TRIANGLES, model.iboTris.numberOfElements,
+				gl.UNSIGNED_SHORT, 0);
+		}
+
+		// Setup rendering lines.
+		var wireframe = (model.fillstyle.search(/wireframe/) != -1);
+		if(wireframe) {
+			gl.disableVertexAttribArray(prog.normalAttrib);
+			gl.vertexAttrib3f(prog.normalAttrib, 0, 0, 0);
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.iboLines);
+			gl.drawElements(gl.LINES, model.iboLines.numberOfElements,
+				gl.UNSIGNED_SHORT, 0);
+		}
+	}
+
+	// App interface.
+	return {
+		start : start
+	}
+
+}());
---
+++ 05_recursive_sphere/vertexDataSphere.js
@@ -0,0 +1,164 @@
+var sphere = ( function() {
+
+	function createVertexData(iterations) {
+
+		// Positions.
+		this.vertices = new Float32Array([
+			1.0, 0.0, 0.0,
+			-1.0, 0.0, 0.0,
+			0.0, 1.0, 0.0,
+			0.0, -1.0, 0.0,
+			0.0, 0.0, 1.0,
+			0.0, 0.0, -1.0
+		]);
+		var vertices = this.vertices;
+		// Normals.
+		this.normals = new Float32Array([
+			1.0, 0.0, 0.0,
+			-1.0, 0.0, 0.0,
+			0.0, 1.0, 0.0,
+			0.0, -1.0, 0.0,
+			0.0, 0.0, 1.0,
+			0.0, 0.0, -1.0
+		]);
+		var normals = this.normals;
+		// Index data.
+		this.indicesLines = new Uint16Array([
+			0, 4, 4, 2, 2, 0,
+			2, 4, 4, 1, 1, 2,
+			1, 4, 4, 3, 3, 1,
+			3, 4, 4, 0, 0, 3,
+			0, 2, 2, 5, 5, 0,
+			2, 1, 1, 5, 5, 2,
+			1, 3, 3, 5, 5, 1,
+			3, 0, 0, 5, 5, 3
+		]);
+		var indicesLines = this.indicesLines;
+		this.indicesTris = new Uint16Array([
+			0, 4, 2,
+			2, 4, 1,
+			1, 4, 3,
+			3, 4, 0,
+			0, 2, 5,
+			2, 1, 5,
+			1, 3, 5,
+			3, 0, 5
+		]);
+		var indicesTris = this.indicesTris;
+		var numberOfRecursions = iterations;
+
+		for(var h = 0; h <= numberOfRecursions; h++)
+		{
+			var numTriangles = this.indicesTris.length / 3;
+			var newNumTriangles = numTriangles * 4;
+			var newIndicesTris = new Int16Array(newNumTriangles * 3);
+			var newIndicesLines = new Int16Array(newNumTriangles * 6);
+			var newVertices = new Float32Array(newIndicesLines.length * 3);
+
+			for(var i = 0; i < numTriangles; i++)
+			{
+				var x = 0, y = 1, z = 2;
+				var iv0, iv1, iv2, v0, v1, v2, a, b, c;
+				iv0 = this.indicesTris[i * 3];
+				iv1 = this.indicesTris[i * 3 + 1]
+				iv2 = this.indicesTris[i * 3 + 2];
+				v0 = vec3.fromValues(this.vertices[iv0 * 3], this.vertices[iv0 * 3 + 1], this.vertices[iv0 * 3 + 2]);
+				v1 = vec3.fromValues(this.vertices[iv1 * 3], this.vertices[iv1 * 3 + 1], this.vertices[iv1 * 3 + 2]);
+				v2 = vec3.fromValues(this.vertices[iv2 * 3], this.vertices[iv2 * 3 + 1], this.vertices[iv2 * 3 + 2]);
+
+				a = vec3.create();
+				b = vec3.create();
+				c = vec3.create();
+
+				vec3.add(a, v0, v2);
+				vec3.scale(a, a, 0.5);
+
+				vec3.add(b, v0, v1);
+				vec3.scale(b, b, 0.5);
+
+				vec3.add(c, v1, v2);
+				vec3.scale(c, c, 0.5);
+				vec3.normalize(a, a);
+				vec3.normalize(b, b);
+				vec3.normalize(c, c);
+
+				var vertixIndex = i * 6;
+				var indexPointTris = i * 12;
+				var indexPointLines = i * 4 * 6;
+
+				newVertices[vertixIndex * 3] = v0[x];
+				newVertices[vertixIndex * 3 + 1] = v0[y];
+				newVertices[vertixIndex * 3 + 2] = v0[z];
+				newVertices[(vertixIndex + 1) * 3] = b[x];
+				newVertices[(vertixIndex + 1) * 3 + 1] = b[y];
+				newVertices[(vertixIndex + 1) * 3 + 2] = b[z];
+				newVertices[(vertixIndex + 2) * 3] = v1[x];
+				newVertices[(vertixIndex + 2) * 3 + 1] = v1[y];
+				newVertices[(vertixIndex + 2) * 3 + 2] = v1[z];
+				newVertices[(vertixIndex + 3) * 3] = c[x];
+				newVertices[(vertixIndex + 3) * 3 + 1] = c[y];
+				newVertices[(vertixIndex + 3) * 3 + 2] = c[z];
+				newVertices[(vertixIndex + 4) * 3] = v2[x];
+				newVertices[(vertixIndex + 4) * 3 + 1] = v2[y];
+				newVertices[(vertixIndex + 4) * 3 + 2] = v2[z];
+				newVertices[(vertixIndex + 5) * 3] = a[x];
+				newVertices[(vertixIndex + 5) * 3 + 1] = a[y];
+				newVertices[(vertixIndex + 5) * 3 + 2] = a[z];
+
+				newIndicesTris[indexPointTris] = vertixIndex;
+				newIndicesTris[indexPointTris + 1] = vertixIndex + 1;
+				newIndicesTris[indexPointTris + 2] = vertixIndex + 5;
+
+				newIndicesTris[indexPointTris + 3] = vertixIndex + 1;
+				newIndicesTris[indexPointTris + 4] = vertixIndex + 2;
+				newIndicesTris[indexPointTris + 5] = vertixIndex + 3;
+
+				newIndicesTris[indexPointTris + 6] = vertixIndex + 5;
+				newIndicesTris[indexPointTris + 7] = vertixIndex + 1;
+				newIndicesTris[indexPointTris + 8] = vertixIndex + 3;
+
+				newIndicesTris[indexPointTris + 9] = vertixIndex + 5;
+				newIndicesTris[indexPointTris + 10] = vertixIndex + 3;
+				newIndicesTris[indexPointTris + 11] = vertixIndex + 4;
+
+				newIndicesLines[indexPointLines] = vertixIndex;
+				newIndicesLines[indexPointLines + 1] = vertixIndex + 1;
+				newIndicesLines[indexPointLines + 2] = vertixIndex + 1;
+				newIndicesLines[indexPointLines + 3] = vertixIndex + 5;
+				newIndicesLines[indexPointLines + 4] = vertixIndex + 5;
+				newIndicesLines[indexPointLines + 5] = vertixIndex;
+
+				newIndicesLines[indexPointLines + 6] = vertixIndex + 1;
+				newIndicesLines[indexPointLines + 7] = vertixIndex + 2;
+				newIndicesLines[indexPointLines + 8] = vertixIndex + 2;
+				newIndicesLines[indexPointLines + 9] = vertixIndex + 3;
+				newIndicesLines[indexPointLines + 10] = vertixIndex + 3;
+				newIndicesLines[indexPointLines + 11] = vertixIndex + 1;
+
+				newIndicesLines[indexPointLines + 12] = vertixIndex + 5;
+				newIndicesLines[indexPointLines + 13] = vertixIndex + 1;
+				newIndicesLines[indexPointLines + 14] = vertixIndex + 1;
+				newIndicesLines[indexPointLines + 15] = vertixIndex + 3;
+				newIndicesLines[indexPointLines + 16] = vertixIndex + 3;
+				newIndicesLines[indexPointLines + 17] = vertixIndex + 5;
+
+				newIndicesLines[indexPointLines + 18] = vertixIndex + 5;
+				newIndicesLines[indexPointLines + 19] = vertixIndex + 3;
+				newIndicesLines[indexPointLines + 20] = vertixIndex + 3;
+				newIndicesLines[indexPointLines + 21] = vertixIndex + 4;
+				newIndicesLines[indexPointLines + 22] = vertixIndex + 4;
+				newIndicesLines[indexPointLines + 23] = vertixIndex + 5;
+			}
+
+			this.vertices = newVertices;
+			this.normals = newVertices;
+			this.indicesTris = newIndicesTris;
+			this.indicesLines = newIndicesLines;
+		}
+	}
+
+	return {
+		createVertexData : createVertexData
+	}
+
+}());
